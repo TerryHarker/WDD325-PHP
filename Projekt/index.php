c7c746a36f1ed8966e595c138bac3b6f5563f172
Terry Harker PHP <terry.harker@bytekultur.net>
2026-04-13 17:10:10 +0200

    Woche 6 - angepasste Projektvorlage

--- Projekt/index.php
+++ Projekt/index.php
@@ -36,7 +36,7 @@ if( is_file("scripts/".$page.".php") == true ){
 	<body>
 		<header class="navbar navbar-expand-md bd-navbar">
 			<nav class="container flex-wrap flex-md-nowrap" aria-label="Main navigation">
-				<a class="navbar-brand p-0 me-2" href="/">NICO's PORTFOLIO<a>
+				<a class="navbar-brand p-0 me-2" href="<?php echo BASEURL; ?>">NICO's PORTFOLIO<a>
 				<ul class="navbar-nav flex-row flex-wrap bd-navbar-nav">
 					<li class="nav-item col-6 col-md-auto">
 						<a class="nav-link p-2 <?php echo $page=='home'? 'active':''; ?>" href="index.php?page=home">Home</a>
@@ -70,9 +70,9 @@ if( is_file("scripts/".$page.".php") == true ){
 		<section class="footer-section">
 			<div class="container">
 				<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-				  <div class="col-md-4 d-flex align-items-center">
-					<a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-					  <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+				  <div class="col-lg-4 d-flex align-items-center">
+					<a href="<?php echo BASEURL; ?>" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+					  NICO's PORTFOLIO
 					</a>
 					<span class="mb-3 mb-md-0 text-body-secondary">© 2024 Nico the webdesigner</span>
 				  </div>
--- Projekt/views/portfolio.php
+++ Projekt/views/portfolio.php
@@ -4,18 +4,34 @@
 		<div class="mt-5">
 				<h2>Aktuelle Projekte</h2>
 		</div>
-		<div class="row mt-4">
-			<div class="col-12 col-sm-6 col-md-3">
+		<div class="row mt-4 mb-5">
+			<div class="col-12 col-sm-6 col-md-3 mb-4">
 				<img src="media/Business-20.png">
+				<div class="caption">
+					<h6 class="mt-2 mb-0">WordPress Business Template</h6>
+					<span>Januar 2026</span>
+				</div>
 			</div>
-			<div class="col-12 col-sm-6 col-md-3">
+			<div class="col-12 col-sm-6 col-md-3 mb-4">
 				<img src="media/eaef_Blurr-402x.jpg">
+				<div class="caption">
+					<h6 class="mt-2 mb-0">Blurr App</h6>
+					<span>Oktober 2025</span>
+				</div>
 			</div>
-			<div class="col-12 col-sm-6 col-md-3">
+			<div class="col-12 col-sm-6 col-md-3 mb-4">
 				<img src="media/Pompeo.jpg">
+				<div class="caption">
+					<h6 class="mt-2 mb-0">Pompeo Webseite</h6>
+					<span>August 2025</span>
+				</div>
 			</div>
-			<div class="col-12 col-sm-6 col-md-3">
+			<div class="col-12 col-sm-6 col-md-3 mb-4">
 				<img src="media/biznus.jpg">
+				<div class="caption">
+					<h6 class="mt-2 mb-0">Biznus Shop Template</h6>
+					<span>April 2025</span>
+				</div>
 			</div>
 		</div>
 		<div>
